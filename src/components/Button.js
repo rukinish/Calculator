b23e4542297e7bd180f62dc31ca9e9f0f5e9a192
rukinish <rukaiyanishfan@gmail.com>
2024-03-20 02:04:47 +0530

    complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import classes from './Button.module.css'
 
-function Button({value}) {
+function Button({value, handler}) {
   return (
-    <div className={value=="C" || value =="=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`}>
+    <div className={value==="C" || value ==="=" ? `${classes.calcBtns} ${classes.res}` : `${classes.calcBtns}`} onClick={()=> handler(value)}>
       {value}
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Home.module.css'
 import Button from './Button'
 
 function Home() {
 
-  const buttons = ["C", "9", "/","8","7","6","*","5","4","3","+","2","1","0","-",".","Del","="];
+  const [res, setRes] = useState("");
 
+  const buttons = ["C","9","/","8","7","6","*","5","4","3","+","2","1","0","-",".","Del","="];
+
+  const findval = () => {
+
+    let result = Function("return "+ res)();
+    setRes(result.toString());
+
+  }
+
+  const handler = (arg) => {
+    if(res === "infinity"){
+      setRes("");
+      return;
+    }
+    if (arg==="C") {
+      setRes("");
+    } else if (arg==="=") {
+      findval();
+    } else if (arg==="Del") {
+      let n = res.length;
+      if (n>0) {
+        setRes(res.slice(0, n-1));
+      }
+    } else {
+      setRes(res.concat(arg));
+    }
+  }
 
   return (
     <div className={classes.home}>
         <div className={classes.inner}> 
             <div className={classes.result}> 
             <div className={classes.box}> 
-                 </div>
+                 {res}</div>
             </div>
       <div className={classes.btn}>
         {buttons.map((ele, index) => {
-          return <Button value={ele} key={index} />
+          return <Button handler={handler} value={ele} key={index} />
         })}
       </div>
     </div>
