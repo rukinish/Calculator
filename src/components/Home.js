1a618ce967e8e9020f86322948e0706ddd88ebaa
rukinish <rukaiyanishfan@gmail.com>
2024-03-20 00:46:27 +0530

    react files and cal base

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='inner'> 
+            <div className='result'> 
+            <div className='box'> 
+                 </div>
+            </div>
+        </div>
+      <div className='btn'></div>
+    </div>
+  )
+}
+
+export default Home
